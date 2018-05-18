1ce0939adf9b19594f6963d63da2997a56fe1814
Tom <tomw0621@gmail.com>
2018-05-17 20:42:24 -0700

    Game code added. Unable to update wrong guesses on screen.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,62 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 var losses = 0;
 var tries = 5;
+var guesses = document.getElementById("failedGuess");
+var guessesTried = ["_", ];
 
-var randoMcLetter = letters[Math.floor(Math.random() * letters.length)];
+function makeRando() {Math.floor(Math.random() * letters.length)};
+var rando = makeRando();
+function randoLetter(i) {letters[i]};
+function updateScore() {
+    document.querySelector("#winCount").innerHTML = wins;
+    document.querySelector("#lossCount").innerHTML = losses
+};
+function updateTries () {
+    document.querySelector("#triesLeft").innerHTML = tries
+};
 
-var userGuess = event.key
 
-if (letter.getID(userGuess) > -1) {
-    if (userGuess === randoMcLetter) {
-        wins++
-    }
 
-    else if (tries == 0) {
-        losses++
-    }
+var randoMcLetter = letters[rando];
 
-    else {
-        // decrease tries by 1
-        tries--
-        // display keys pressed
-    }
-}
+updateScore();
+updateTries();
+randoLetter(rando);
+
+document.onkeyup = function(event) {   
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess)
+    if (letters.indexOf(userGuess) > -1) {
+        if (userGuess === randoMcLetter) {
+            wins++;
+            alert("Correct!")
+            updateScore();
+            guessesTried.clear();
+            tries = 5;
+            rando++;
+            document.querySelector("#failedGuess").innerHTML = " ";
+        }
+
+        else if (tries == 1) {
+            alert("No tries remaining! You lose!");
+            losses++;
+            updateScore();
+            tries = 5;
+        }
+
+        else {
+            // decrease tries by 1 and updates tries remaining count.
+            tries--;
+            alert("Incorrect! Tries remaining: "+ tries);
+            updateTries();
+            // display keys pressed
+            guessesTried.push(userGuess);
+            for (var i=0; i < guesses.length; i++) {
+                var userGuessP = document.createElement("p");
+                userGuessP.textContent = guesses[i];
+                guesses.appendChild(userGuessP);
+            } ;
+            
+        };
+    };
+};
